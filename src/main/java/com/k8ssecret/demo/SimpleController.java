1b687f9026e3ebccb7807e026045139bcc75da90
Tony-Hu <tony.hu1213@gmail.com>
2019-03-07 14:13:21 -0800

    Update to read property from local, or from env var if available

--- src/main/java/com/k8ssecret/demo/SimpleController.java
+++ src/main/java/com/k8ssecret/demo/SimpleController.java
@@ -1,13 +1,29 @@
 package com.k8ssecret.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleController {
 
+    @Autowired
+    Environment env;
+
     @GetMapping("/")
-    public String hello() {
-        return System.getenv("TEST_ENV");
+    public String getProperties() {
+        StringBuilder sb = new StringBuilder();
+        String userName = System.getenv("MY_USERNAME");
+        String password = System.getenv("MY_PASSWORD");
+        if (userName == null) {
+            userName = env.getProperty("my.username");
+        }
+        if (password == null) {
+            password = env.getProperty("my.password");
+        }
+        sb.append("The name is: \"").append(userName).append("\"\n");
+        sb.append("The password is: \"").append(password).append("\"\n");
+        return sb.toString();
     }
 }
