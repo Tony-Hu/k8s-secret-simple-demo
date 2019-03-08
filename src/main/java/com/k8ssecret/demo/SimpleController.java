48a81728414e469f540777a630265cacfa3bd092
Tony-Hu <tony.hu1213@gmail.com>
2019-03-07 22:55:00 -0800

    Simplify code logic

--- src/main/java/com/k8ssecret/demo/SimpleController.java
+++ src/main/java/com/k8ssecret/demo/SimpleController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Base64;
+import java.util.Map;
 
 @RestController
 public class SimpleController {
@@ -15,16 +16,13 @@ public class SimpleController {
 
     @GetMapping("/")
     public String getProperties() {
+        Map<String, String> environmentVariables = System.getenv();
+
+        String userName = environmentVariables.getOrDefault("MY_USERNAME", env.getProperty("my.username"));
+        String password = environmentVariables.getOrDefault("MY_PASSWORD", env.getProperty("my.password"));
+        String base64Decode = environmentVariables.getOrDefault("BASE64_STRING", new String(Base64.getDecoder().decode(env.getProperty("my.base64Encode"))));
+
         StringBuilder sb = new StringBuilder();
-
-        String userName = System.getenv("MY_USERNAME");
-        String password = System.getenv("MY_PASSWORD");
-        String base64Decode = System.getenv("BASE64_STRING");
-
-        userName = userName == null ? env.getProperty("my.username") : userName;
-        password = password == null ? env.getProperty("my.password") : password;
-        base64Decode = base64Decode == null ? new String(Base64.getDecoder().decode(env.getProperty("my.base64Encode"))) : base64Decode;
-
         sb.append("The name is: \"").append(userName).append("\"<br>");
         sb.append("The password is: \"").append(password).append("\"<br>");
         sb.append("The base 64 decoded string is: \"").append(base64Decode).append("\"<br>");
