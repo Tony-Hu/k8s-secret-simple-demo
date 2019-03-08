ead56b45cf883f88940ee926b9f8d16146932845
Tony-Hu <tony.hu1213@gmail.com>
2019-03-07 16:34:35 -0800

    Add a test case for base 64 stored value

--- src/main/java/com/k8ssecret/demo/SimpleController.java
+++ src/main/java/com/k8ssecret/demo/SimpleController.java
@@ -5,6 +5,8 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Base64;
+
 @RestController
 public class SimpleController {
 
@@ -14,16 +16,19 @@ public class SimpleController {
     @GetMapping("/")
     public String getProperties() {
         StringBuilder sb = new StringBuilder();
+
         String userName = System.getenv("MY_USERNAME");
         String password = System.getenv("MY_PASSWORD");
-        if (userName == null) {
-            userName = env.getProperty("my.username");
-        }
-        if (password == null) {
-            password = env.getProperty("my.password");
-        }
-        sb.append("The name is: \"").append(userName).append("\"\n");
-        sb.append("The password is: \"").append(password).append("\"\n");
+        String base64Decode = System.getenv("BASE64_STRING");
+
+        userName = userName == null ? env.getProperty("my.username") : userName;
+        password = password == null ? env.getProperty("my.password") : password;
+        base64Decode = base64Decode == null ? new String(Base64.getDecoder().decode(env.getProperty("my.base64Encode"))) : base64Decode;
+
+        sb.append("The name is: \"").append(userName).append("\"<br>");
+        sb.append("The password is: \"").append(password).append("\"<br>");
+        sb.append("The base 64 decoded string is: \"").append(base64Decode).append("\"<br>");
+
         return sb.toString();
     }
 }
