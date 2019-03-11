d573fcf76b1bb4419c7ae90d5f3372f40745a762
Tony-Hu <tony.hu1213@gmail.com>
2019-03-11 11:31:51 -0700

    Modify program logic

--- src/main/java/com/k8ssecret/demo/SimpleController.java
+++ src/main/java/com/k8ssecret/demo/SimpleController.java
@@ -1,27 +1,23 @@
 package com.k8ssecret.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Base64;
-import java.util.Map;
 
 @RestController
 public class SimpleController {
 
-    @Autowired
-    Environment env;
+    @Value("${my.username}")
+    private String userName;
+
+    @Value("${my.password}")
+    private String password;
+
+    @Value("${my.base64Encode}")
+    private String base64Decode;
 
     @GetMapping("/")
     public String getProperties() {
-        Map<String, String> environmentVariables = System.getenv();
-
-        String userName = environmentVariables.getOrDefault("MY_USERNAME", env.getProperty("my.username"));
-        String password = environmentVariables.getOrDefault("MY_PASSWORD", env.getProperty("my.password"));
-        String base64Decode = environmentVariables.getOrDefault("BASE64_STRING", new String(Base64.getDecoder().decode(env.getProperty("my.base64Encode"))));
-
         StringBuilder sb = new StringBuilder();
         sb.append("The name is: \"").append(userName).append("\"<br>");
         sb.append("The password is: \"").append(password).append("\"<br>");
